ac8409abbaf6fe581d501392178b28e0ca4897ba
Aline Damião <aline.isilvadamiao@gmail.com>
2023-07-18 20:41:31 -0300

    tratamento de erro - busca não encontrada

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -9,12 +9,15 @@ async function buscarVideo(evento) {
 
     const lista = document.querySelector("[data-lista]");
 
-    while (lista.firstChild){
+    while (lista.firstChild) {
         lista.removeChild(lista.firstChild);
     }
 
     busca.forEach(elemento => lista.appendChild(
         constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+    if (busca.length == 0) {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com esse termo.</h2>`
+    }
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
